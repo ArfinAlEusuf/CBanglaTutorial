4d7f298fe1ab85a39eb9db84fc17882aa13974ba
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-09-20 11:08:40 +0600

    printing

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello world\n");
+    printf("i am md arfin al eusuf");
+    return 0;
+}
+
+/**
+ * editing
+ * compiling
+ * Linking
+ * Exicuting 
+*/
---
+++ print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("my name is md arfin al eusuf");
+    return 0;
+}
