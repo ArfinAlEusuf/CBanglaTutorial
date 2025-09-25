fdf4d05f7ab5e20668494adc73b2cc324b067897
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-09-25 16:10:46 +0600

    dataType

---
+++ dataType.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int num = 65;
+    float num2= 45.6;
+    char ch = 'T';
+
+    num = num2;
+
+    printf("%d \n",num);
+    printf("%f \n",num2);
+    printf("%c \n",ch);
+
+    return 0;
+}
---
+++ variable.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+
+int main(){
+    int variable = 45;
+    printf("variable + 5 = %d, variable + 5"); 
+    printf("variable - 5 = %d, variable + 5"); 
+    printf("variable * 5 = %d, variable + 5"); 
+    printf("variable / 5 = %d, variable + 5"); 
+    return 0;
+}
