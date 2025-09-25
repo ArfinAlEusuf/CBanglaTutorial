bddfd346c1c018b2089e461dcd46933ad49e4205
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-09-25 16:30:00 +0600

    input

---
+++ input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int a , b ;
+    float c, d;
+
+    scanf("%d %d", &a, &b);
+    printf("%d %d\n", a);
+
+    scanf("%f %f", &c, &d);
+    printf("%f %f", c ,d);
+
+    return 0;
+}
